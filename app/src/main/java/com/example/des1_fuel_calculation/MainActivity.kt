5e8621594b6f92a581344fedd401b069235c901f
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-03-26 08:51:26 -0300

    Algumas alterações na parte mais lógica do app

--- app/src/main/java/com/example/des1_fuel_calculation/MainActivity.kt
+++ app/src/main/java/com/example/des1_fuel_calculation/MainActivity.kt
@@ -1,29 +1,43 @@
 package com.example.des1_fuel_calculation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnIniciar = findViewById<Button>(R.id.btn_iniciar)
+        btnIniciar.setOnClickListener {
+            val intent = Intent(this, preco::class.java)
+            startActivity(intent)
         }
+
     }
 }
-
-
 /*
-* 1 - tela de "boas vindas"
-* 2 - tela do preço do combustível
-* 3 - tela do consumo do carro por litro
-* 4 - Tela de distancia até o destino
-* 5 - Tela que mostra o resultado
-* */
+1 - Pequenos ajustes (icones) nas telas
+2 - Conexões entre as telas
+3 - Mostrar resultado final
+4 - Compilar programa
+5 - Ver como colocar um vídeo no github
+*/
+
+//            if (preco == "") {
+//                Snackbar.make(edtPreco, "Preencha o preço do combustível", Snackbar.LENGTH_LONG)
+//                    .show()
+//            }
+//mudar de tela
+//intent -
+
+//Recuperar os componentes editText
+//    Criar 01 variável e associar o componente de UI (edittext-input)
+//    val edtConsumo = findViewById<TextInputEditText>(R.id.consumo)
+//    val edtDistancia = findViewById<TextInputEditText>(R.id.distancia)
