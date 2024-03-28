4d6bec420bdbfbc1f6014745db9b3a9a6b8bfdf8
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-03-28 08:52:52 -0300

    Toques semifinais no app

--- app/src/main/java/com/example/des1_fuel_calculation/resultado.kt
+++ app/src/main/java/com/example/des1_fuel_calculation/resultado.kt
@@ -1,5 +1,6 @@
 package com.example.des1_fuel_calculation
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -15,19 +16,29 @@ class resultado : AppCompatActivity() {
         val preco = intent.getFloatExtra(KEY_PRECO, 0f)
         val dist = intent.getFloatExtra(KEY_DIST, 0f)
         val cons = intent.getFloatExtra(KEY_CONS, 0f)
-        val texto: String = "$preco\n\n$dist\n\n$cons"
+        val precoSTR: String = when (preco) {
+            in 0.00f..9.99f -> "0${"%.1f".format(preco)}"
+            else -> "${"%.1f".format(preco)}"
+        }
+        val distSTR: String = when (dist) {
+            in 0.00f..9.99f -> "0${"%.1f".format(dist)}"
+            else -> "${"%.1f".format(dist)}"
+        }
+        val consSTR: String = when (cons) {
+            in 0.00f..9.99f -> "0${"%.1f".format(cons)}"
+            else -> "${"%.1f".format(cons)}"
+        }
+        val texto: String = "$precoSTR\n\n$consSTR\n\n$distSTR"
         val resultado: Float = (dist / cons) * preco
         val btn_novo = findViewById<Button>(R.id.butao)
         val textV = findViewById<TextView>(R.id.resumo2)
         val textR = findViewById<TextView>(R.id.texto_final)
         textV.text = texto
-        textR.text = "Custo Total\nR$ $resultado"
-
-
+        textR.text = "Custo Total\nR$ ${"%.2f".format(resultado)}"
 
         btn_novo.setOnClickListener {
-
-
+            val intent = Intent(this, com.example.des1_fuel_calculation.preco::class.java)
+            startActivity(intent)
         }
     }
 }
