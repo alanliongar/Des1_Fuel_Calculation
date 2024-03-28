8bf2a10892ea198aabc72da1c8cb7771ee2463d4
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-03-28 11:33:21 -0300

    Toques finais no app

--- app/src/main/java/com/example/des1_fuel_calculation/preco.kt
+++ app/src/main/java/com/example/des1_fuel_calculation/preco.kt
@@ -3,6 +3,7 @@ package com.example.des1_fuel_calculation
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -16,7 +17,12 @@ class preco : AppCompatActivity() {
         setContentView(R.layout.activity_preco)
         val btn_prox = findViewById<Button>(R.id.butaoP)
         val edtPreco = findViewById<TextInputEditText>(R.id.precoP)
+        val btn_voltar_p = findViewById<ImageButton>(R.id.voltar_p)
 
+        btn_voltar_p.setOnClickListener {
+            val intentV = Intent(this, MainActivity::class.java)
+            startActivity(intentV)
+        }
         btn_prox.setOnClickListener {
             if (edtPreco.text.toString().isBlank()) {
                 Snackbar.make(edtPreco, "Preencha o preço do combustível", Snackbar.LENGTH_LONG)
